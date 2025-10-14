476dce718d0b5677021204f1d7167813cee10379
J-Anteskog <anteskogen85@gmail.com>
2025-10-14 16:30:42 +0200

    Add privacy policy pages and update navigation links for compliance

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_sv, name='home_sv'),
     path('en/', views.home_en, name='home_en'),
+    path("integritetspolicy/", views.privacy_policy_sv, name="privacy_policy_sv"),
+    path("en/privacy-policy/", views.privacy_policy_en, name="privacy_policy_en"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,10 @@ def home_sv(request):
 
 def home_en(request):
     return render(request, 'en/home.html')
+
+
+def privacy_policy_sv(request):
+    return render(request, "sv/privacy_policy.html")
+
+def privacy_policy_en(request):
+    return render(request, "en/privacy_policy.html")
