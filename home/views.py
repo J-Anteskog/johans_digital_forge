a20e88da087712253f01d7dd78fcac526ef4f1b4
J-Anteskog <anteskogen85@gmail.com>
2025-10-20 19:45:17 +0200

    Refactor code structure for improved readability and maintainability

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from portfolio.models import Project
+
 
 def home_sv(request):
-    return render(request, 'sv/home.html')
+    projects = Project.objects.filter(is_active=True)[:3]
+    return render(request, 'sv/home.html', {'projects': projects})
 
 def home_en(request):
-    return render(request, 'en/home.html')
-
+    projects = Project.objects.filter(is_active=True)[:3]
+    return render(request, 'en/home.html', {'projects': projects})
 
 def privacy_policy_sv(request):
     return render(request, "sv/privacy_policy.html")
