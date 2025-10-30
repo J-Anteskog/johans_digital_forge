6d69bce2fcc8dc730d136eae18af8b80c46301bf
j-anteskog <anteskogen85@gmail.com>
2025-10-30 20:27:47 +0100

    Byt till psycopg3 för PostgreSQL på Render

--- johans_digital_forge/settings.py
+++ johans_digital_forge/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import environ
 import dj_database_url
+import psycopg
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
