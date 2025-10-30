230934e709089ad923a605c18f4b55c0a91f0181
j-anteskog <anteskogen85@gmail.com>
2025-10-30 20:42:20 +0100

    Ta bort vyer för att skapa admin-användare och importera data, samt lägg till data.json i .gitignore

--- home/views.py
+++ home/views.py
@@ -1,26 +1,5 @@
 from django.shortcuts import render
 from portfolio.models import Project
-from django.contrib.auth.models import User
-from django.core.management import call_command
-from django.http import HttpResponse
-import os
-
-def import_data_view(request):
-    data_file = os.path.join(os.path.dirname(__file__), '..', 'data.json')
-    try:
-        call_command('loaddata', data_file)
-        return HttpResponse("✅ Data importerad från data.json!")
-    except Exception as e:
-        return HttpResponse(f"⚠️ Fel vid import: {e}")
-
-
-
-def create_admin_user():
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "Admin123!")
-        print("✅ Superuser created: admin / Admin123!")
-
-
 
 
 def home_sv(request):
@@ -36,10 +15,3 @@ def privacy_policy_sv(request):
 
 def privacy_policy_en(request):
     return render(request, "en/privacy_policy.html")
-
-def create_admin_view(request):
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "Admin123!")
-        return HttpResponse("✅ Superuser skapad: admin / Admin123!")
-    else:
-        return HttpResponse("⚠️ Admin finns redan.")
